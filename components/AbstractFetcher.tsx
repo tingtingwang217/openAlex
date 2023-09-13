528538410d24a714ee34db4a45125815a1cc020a
wangtingting217 <wangtingting217@hotmail.com>
2023-09-13 01:19:29 -0400

    update

--- components/AbstractFetcher.tsx
+++ components/AbstractFetcher.tsx
@@ -7,16 +7,70 @@ function AbstractFetcher() {
   const [doi, setDoi] = useState('');
   const [abstract, setAbstract] = useState('');
 
+  // https://api.openalex.org/works/W2741809807
+  // https://api.openalex.org/works/${doi}
+
+  // const fetchAbstract = async () => {
+  //   try {
+  //     const response = await axios.get(`https://api.openalex.org/works/${doi}`);
+  //     console.log(response)
+  //     const { abstract_inverted_index } = response.data;
+
+  //     // Implement the decompression logic for abstract_inverted_index if needed
+  //     setAbstract(abstract_inverted_index);
+  //   } catch (error) {
+  //     console.error('Error fetching abstract:', error);
+  //   }
+  // };
+
+
+
+  // const fetchAbstract = async () => {
+  //   try {
+  //     const response = await axios.get(`https://api.openalex.org/works/${doi}`);
+  //     console.log(response);
+  //     const { abstract_inverted_index } = response.data;
+  
+  //     if (typeof abstract_inverted_index === 'string') {
+  //       // Implement the decompression logic for abstract_inverted_index if needed
+  //       setAbstract(abstract_inverted_index);
+  //     } else {
+  //       console.error('Invalid abstract data:', abstract_inverted_index);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error fetching abstract:', error);
+  //   }
+  // };
+
+
+
   const fetchAbstract = async () => {
     try {
       const response = await axios.get(`https://api.openalex.org/works/${doi}`);
+      console.log(response);
       const { abstract_inverted_index } = response.data;
-      // Implement the decompression logic for abstract_inverted_index if needed
-      setAbstract(abstract_inverted_index);
+  
+      // Check if abstract_inverted_index is an object (not a string)
+      if (typeof abstract_inverted_index === 'object') {
+        // You might need to convert the object to a string here
+        // For example, using JSON.stringify()
+        const abstractString = JSON.stringify(abstract_inverted_index);
+  
+        // Implement any further processing or decompression logic as needed
+        setAbstract(abstractString);
+      } else if (typeof abstract_inverted_index === 'string') {
+        // If it's already a string, set it directly
+        setAbstract(abstract_inverted_index);
+      } else {
+        console.error('Invalid abstract data:', abstract_inverted_index);
+      }
     } catch (error) {
       console.error('Error fetching abstract:', error);
     }
   };
+  
+  
+
 
   return (
     <div>

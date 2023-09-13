a413e8e0181f35250cefdb9cbb0cdd55d3297cc8
wangtingting217 <wangtingting217@hotmail.com>
2023-09-13 00:16:17 -0400

    project

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+
+import AbstractFetcher from '../components/AbstractFetcher';
+
+function Home() {
+  return (
+    <div>
+      <h1>Research Paper Abstract Fetcher</h1>
+      <AbstractFetcher />
+    </div>
+  );
+}
+
+export default Home;
